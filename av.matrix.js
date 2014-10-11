76ea0e9a2e354de4255b719add3ae69141a53a60
Abel Vázquez Montoro <AbelVM@users.noreply.github.com>
2014-10-11 13:23:20 +0200

    Some language style fixes and typo/bug fix in .samesize() method

--- av.matrix.js
+++ av.matrix.js
@@ -2,7 +2,7 @@
 *:: av.matrix ::
 *2D arrays objects & Algebra
 *@author	Abel Vázquez <abel.vm@gmail.com>
-*@version	1.0.3
+*@version	1.0.4
 *@since	2014-10-10
 */
 
@@ -41,7 +41,7 @@
 			return [this.length,this[0].length || 1];
 		};
 		_m.trace = function(){
-			if (_m.isNaN()){console.error("Matrix:trace: Matrix must be numerical"); return NaN};
+			if (_m.isNaN()){console.error("Matrix:trace: Matrix must be numerical"); return NaN}
 			return this.reduce(function(a,b,c){return b.reduce(function(d,e,f){return d+(c==f)?e:0},a)},0);
 		};
 		_m.item = function(i,j){
@@ -57,7 +57,7 @@
 
 		// process methods
 		_m.fill = function(value) {
-			if (isNaN(value)){console.error("Matrix:fill: Arguments must be numerical"); return Matrix()};
+			if (isNaN(value)){console.error("Matrix:fill: Arguments must be numerical"); return Matrix()}
 			value = value || 1;
 			return Matrix(this.map(function(a){return a.map(function(){return value})})); 
 		};
@@ -77,7 +77,7 @@
 			return Matrix(_final.map(function(a,b){return a.map(function(c,d){return that[d][b]})}));
 		};
 		_m.minor = function(i,j){
-			if (this.size().some(function(a){a<2})) {console.error("Matrix:submatrix: vectors don't have submatrices");return Matrix();}
+			if (this.size().some(function(a){return a<2})) {console.error("Matrix:submatrix: vectors don't have submatrices");return Matrix();}
 			var	_size = this.size,
 					_final = new Matrix(_size[0]-1, _size[1]-1),
 					_result = _final.map(function(a,b){return a.map(function(c,d){b=(b<i)?b:b+1;d=(d<j)?d:d+1;return _m[b][d];})});
@@ -86,18 +86,18 @@
 		
 		// algebra methods
 		_m.scalar = function(value) {
-			if (_m.isNaN()){console.error("Matrix:scalar: Matrix must be numerical"); return Matrix()};
+			if (_m.isNaN()){console.error("Matrix:scalar: Matrix must be numerical"); return Matrix()}
 			value = value || 1;
 			return this.map(function(a){return a.map(function(b){return b*value})}); 
 		};
 		_m.add = function (arrB){
-			if (_m.isNaN()||arrB.isNaN()){console.error("Matrix:add: Both Matrices must be numerical"); return Matrix()};
+			if (_m.isNaN()||arrB.isNaN()){console.error("Matrix:add: Both Matrices must be numerical"); return Matrix()}
 			var s = (this.samesize( arrB))? this.map(function(a,b){return a.map(function(c,d){return c+arrB[b][d]})}):[];
-			if (s==[]) {console.error("Matrix:add: Dimenssions don't fit"); return Matrix()};
+			if (s==[]) {console.error("Matrix:add: Dimenssions don't fit"); return Matrix()}
 			return Matrix(s);
 		};
 		_m.dot = function(arrB){
-			if (_m.isNaN()||arrB.isNaN()){console.error("Matrix:dot: Both Matrices must be numerical"); return Matrix()};
+			if (_m.isNaN()||arrB.isNaN()){console.error("Matrix:dot: Both Matrices must be numerical"); return Matrix()}
 			if (_m.size()[1]!=arrB.size()[0]){console.error("Matrix:dot: Dimenssions don't fit");return Matrix();}
 			var 	that = this,
 					_final= new Matrix(_m.size()[0],arrB.size()[1]),
@@ -107,7 +107,7 @@
 		_m.LU = function(){
 			if (!this.square()||this.isNaN()){console.error("Matrix:det: Input must be an square numerical matrix"); return Matrix();}
 			var	n = this.size()[0],
-					_L = new Matrix.diagonal(n),
+					_L = new Matrix.Diagonal(n),
 					_U = this.clone();
 			for (var k =0; k<n-1;k++){
 				for (var i = k+1; i<n;i++){
@@ -127,9 +127,9 @@
 		};
 		_m.adjugate = function(){
 			if (!this.square()||this.isNaN()){console.error("Matrix:cofactor: Input must be an square numerical matrix"); return Matrix();}
-			var	that=this;
+			var	that=this,
 					_size=this.size()[0],
-					_final = new Matrix.fill(_size);
+					_final = new Matrix.Fill(_size);
 			return Matrix(_final.map(function(a,b){return a.map(function(c,d){return Math.pow(-1,(b+d))*that.minor(b,d).det()})}));
 		};
 		_m.inverse = function(){
@@ -141,13 +141,13 @@
 		
 		// checking methods 
 		_m.samesize = function(arrB){
-			var ad =a.size(), bd = b.size();
+			var ad =this.size(), bd = arrB.size();
 			return ad[0]==bd[0] && ad[1]==bd[1];
 		};		
 		_m.is = function(arrB) {
 			if (this === arrB) return true;
 			if ((this == null || arrB== null) || !this.samesize(arrB)) return false;
-			return this.reduce(function(a,b,c,d){return a && b.reduce(function(e,f,g,h){return e && f == arrB[c][g]},true)},true);
+			return this.reduce(function(a,b,c){return a && b.reduce(function(e,f,g){return e && f == arrB[c][g]},true)},true);
 		};
 		_m.isNaN = function(){
 			return Matrix.isNaN(this);
@@ -167,31 +167,29 @@
 		_m.tostring = function(sep){
 			sep = sep || ', ';
 			return (_m.size()[1]==1)?this.join(sep):_m.reduce(function(a,b){return a+b.join(sep)+'\n'},'');
-		}
-		
+		};
 		// finally
 		return _m;
-	}	
-
+	};
 	//functions
-	Matrix.fill = function(dim, value) {
-		if (isNaN(dim)||isNaN(value)){console.error("Matrix:fill: Arguments must be numerical"); return Matrix()};
+	Matrix.Fill = function(dim, value) {
+		if (isNaN(dim)||isNaN(value)){console.error("Matrix:fill: Arguments must be numerical"); return Matrix()}
 		value = value || 1;
 		return Matrix(Matrix(dim,dim).map(function(a){return a.map(function(){return value})})); 
 	};
-	Matrix.diagonal = function(dim, value) {
+	Matrix.Diagonal = function(dim, value) {
 		value = value || 1;
-		if (isNaN(dim)||isNaN(value)){console.error("Matrix:diagonal: Arguments must be numerical"); return Matrix()};
+		if (isNaN(dim)||isNaN(value)){console.error("Matrix:diagonal: Arguments must be numerical"); return Matrix()}
 		return new Matrix(Matrix(dim,dim).map(function(a,b){return a.map(function(c, d){return (b==d)? value:0})})); 
 	};
-	Matrix.upper = function(dim, value) {
+	Matrix.Upper = function(dim, value) {
 		value = value || 1;
-		if (isNaN(dim)||isNaN(value)){console.error("Matrix:upper: Arguments must be numerical"); return Matrix()};
+		if (isNaN(dim)||isNaN(value)){console.error("Matrix:upper: Arguments must be numerical"); return Matrix()}
 		return new Matrix(Matrix(dim,dim).map(function(a,b){return a.map(function(c, d){return (b>=d)? value:0})})); 	
 	};
-	Matrix.lower = function(dim, value) {
+	Matrix.Lower = function(dim, value) {
 		value = value || 1;
-		if (isNaN(dim)||isNaN(value)){console.error("Matrix:lower: Arguments must be numerical"); return Matrix()};
+		if (isNaN(dim)||isNaN(value)){console.error("Matrix:lower: Arguments must be numerical"); return Matrix()}
 		return new Matrix(Matrix(dim,dim).map(function(a,b){return a.map(function(c, d){return (b<=d)? value:0})})); 	
 	};
 	Matrix.isNaN = function(e){
